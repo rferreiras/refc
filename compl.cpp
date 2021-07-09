0080a32c527e473243c9bf88e0c2bb5a99044b4e
Roman erreiras <referreiras@outlook.com>
2021-07-09 15:17:08 -0400

    Primer commit
    123

---
+++ compl.cpp
@@ -0,0 +1,14 @@
+/*transpiller to c*/	
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stdio.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	string file="ex\\e.ref";
+
+	return 0;
+}
